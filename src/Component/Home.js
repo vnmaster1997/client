0d1f2eb2cbb7f77a84ca04139b89a19c859e5281
Nam Nguyen <namnv537@gmail.com>
2019-07-22 19:28:06 +0700

    .

---
+++ src/Component/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+    return() {
+        render(
+            <div>Home</div>
+            <div>SHOP</div>
+            <img></img>
+        )
+    }
+}
